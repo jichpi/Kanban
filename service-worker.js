603ee917519237e99ed1682068a47356ab8aeb4f
jichpi <maxiou@msn.com>
2025-08-15 17:07:15 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,20 @@
-// Simple cache-first PWA service worker
-const CACHE = 'ocr-etiquettes-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  'https://unpkg.com/tesseract.js@5.1.0/dist/tesseract.min.js'
+const CACHE='labels-pwa-v3';
+const ASSETS=[
+  './','./index.html','./manifest.webmanifest','./service-worker.js',
+  './icons/icon-192.png','./icons/icon-512.png',
+  'https://unpkg.com/tesseract.js@5.1.0/dist/tesseract.min.js',
+  'https://unpkg.com/@zxing/browser@0.1.5/umd/index.min.js'
 ];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => (k!==CACHE)&&caches.delete(k))))
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  const url = new URL(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(resp => {
-      return resp || fetch(e.request).then(networkResp => {
-        // Optionnel : mettre en cache les nouvelles requêtes même hors liste blanche locale
-        if (e.request.method === 'GET' && url.origin === location.origin) {
-          const respClone = networkResp.clone();
-          caches.open(CACHE).then(cache => cache.put(e.request, respClone));
-        }
-        return networkResp;
-      }).catch(() => {
-        // Offline fallback minimal (pour HTML racine)
-        if (e.request.mode === 'navigate') {
-          return caches.match('./index.html');
-        }
-      });
-    })
-  );
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))));});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+    const u=new URL(e.request.url);
+    if(e.request.method==='GET' && u.origin===location.origin){
+      const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy));
+    }
+    return resp;
+  }).catch(()=>{
+    if(e.request.mode==='navigate') return caches.match('./index.html');
+  })));
 });
